641e09ca6cf69582f29e543c9482d564e646b96d
saurabh.unde <saurabh.unde@vodafone.com>
2024-05-27 15:46:15 +0530

    camera selection removed

--- src/Scanner.jsx
+++ src/Scanner.jsx
@@ -3,56 +3,57 @@ import { useState, useCallback, useMemo, useEffect, useRef } from 'react'
 
 const Scanner = () => {
     //const devices = useDevices();
-    const [devices, setDevices] = useState([]);
-    const [deviceId, setDeviceId] = useState(undefined);
+   // const [devices, setDevices] = useState([]);
+    //const [deviceId, setDeviceId] = useState(undefined);
     const [result, setResult] = useState([]);
     const [isScanning, setIsScanning] = useState(false);
-    const selectElement = useRef(null)
+    //const selectElement = useRef(null)
     const onScanComplete = useCallback((result) => {
         console.log(result);
         setResult(result[0].rawValue)
     }, []);
 
-    const fetchDevices = async () => {
-      try {
-        await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        const inputs = await navigator.mediaDevices.enumerateDevices();
-        console.log("devices---> ", inputs);
-        if (!inputs.length) {
-          throw new Error("No devices found.");
-        }
-        const devicesTemp = inputs.filter(
-          (device) => device.kind === "videoinput"
-        );
+    // const fetchDevices = async () => {
+    //   try {
+    //     await navigator.mediaDevices.getUserMedia({
+    //       video: true,
+    //     });
+    //     const inputs = await navigator.mediaDevices.enumerateDevices();
+    //     console.log("devices---> ", inputs);
+    //     if (!inputs.length) {
+    //       throw new Error("No devices found.");
+    //     }
+    //     const devicesTemp = inputs.filter(
+    //       (device) => device.kind === "videoinput"
+    //     );
 
-        console.log(selectElement);
-        if(selectElement.current && selectElement.current) { 
-            selectElement.current.options.length = 0;
-            const firstOption = document.createElement('option');
-            firstOption.value = '';
-            firstOption.textContent = 'Select a Device...';
-            selectElement.current.appendChild(firstOption);
-            devicesTemp.forEach((device) => {
-              const option = document.createElement('option');
-              option.value = device.deviceId;
-              option.textContent = `${device.label}`;
-              selectElement.current.appendChild(option);
-            });
-        }
-        setDevices(devicesTemp);
-        setDeviceId(devicesTemp[0].deviceId);
-      } catch (error) {
-        console.error("Error fetching devices:", error);
-      }
-    };
+    //     console.log(selectElement);
+    //     if(selectElement.current && selectElement.current) { 
+    //         selectElement.current.options.length = 0;
+    //         const firstOption = document.createElement('option');
+    //         firstOption.value = '';
+    //         firstOption.textContent = 'Select a Device...';
+    //         selectElement.current.appendChild(firstOption);
+    //         devicesTemp.forEach((device) => {
+    //           const option = document.createElement('option');
+    //           option.value = device.deviceId;
+    //           option.textContent = `${device.label}`;
+    //           selectElement.current.appendChild(option);
+    //         });
+    //     }
+    //     console.log("devices--->", devicesTemp)
+    //     setDevices(devicesTemp);
+    //     setDeviceId(devicesTemp[0].deviceId);
+    //   } catch (error) {
+    //     console.error("Error fetching devices:", error);
+    //   }
+    // };
       
-    useEffect(() => {
-        if(devices && !devices.length) {
-            fetchDevices()
-        }
-    }, [devices])
+    // useEffect(() => {
+    //     if(devices && !devices.length) {
+    //         fetchDevices()
+    //     }
+    // }, [devices])
     
     const formats = useMemo(() => {
       return [
@@ -86,16 +87,16 @@ const Scanner = () => {
       <button onClick={() => setIsScanning(!isScanning)}>
         {!isScanning ? "Stop Scanning" : "Start Scanning"}
       </button>
-      <select ref={selectElement} id={"deviceList"} onChange={(e) => setDeviceId(e.target.value)}>
-        {/* <option value={undefined}>Select a device</option>
+      {/* <select ref={selectElement} id={"deviceList"} onChange={(e) => setDeviceId(e.target.value)}>
+        <option value={undefined}>Select a device</option>
         {devices.map((device, index) => (
           <option key={index} value={device.deviceId}>
             {device.label}
           </option>
-        ))} */}
-      </select>
+        ))}
+      </select> */}
 
-      {devices && devices.length > 0 && <ReactScanner
+      {<ReactScanner
         onScan={onScanComplete}
         allowMultiple={true}
         paused={isScanning}
@@ -121,7 +122,7 @@ const Scanner = () => {
           tracker: boundingBox,
         }}
         constraints={{
-          deviceId: deviceId,
+          //deviceId: deviceId,
           facingMode: "environment",
         }}
         scanDelay={2000}
